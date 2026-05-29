2b8a97292375613b5322d944de22cb1f30e35041
AccurateEgg <ian.rodriguez@ht-la.org>
2026-05-29 14:29:58 -0700

    BAG.java

--- BAG.java
+++ BAG.java
@@ -4,18 +4,18 @@ public class Bag extends Bag {
   }
 }
 
+public class Handbag extends Bag {
+  public Handbag() {
+    super(20);
+  }
+}
+
 public class Backpack extends Bag {
   public Backpack() {
     super(80);
   }
 }
 
-public class Handbag extends Bag {
-  public Handbag() {
-    super(20);
-  }
-}
-  
 public class Wallet extends Bag {
   public Wallet() {
     super(5);
