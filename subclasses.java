7cbb32bac788cb30b84e5a9db442c43c33d3bcca
AccurateEgg <ian.rodriguez@ht-la.org>
2026-05-29 13:00:22 -0700

    subclasses.java

--- subclasses.java
+++ subclasses.java
@@ -1,15 +1,15 @@
-public class Things extends Item {
+public class Stuff extends Item {
     private int damage;
 
-    public Things(String name, String description, double weight, int damage) {
-        super(name, description, weight, true, "Things");
+    public stuff(String name, String description, double weight, int damage) {
+        super(name, description, weight, true);
         this.damage = damage;
     }
 public class Eating extends Item {
     private int nutrients;
 
     public Eating(String name, String description, double weight, int nutrients) {
-        super(name, description, weight, true, "Eating");
+        super(name, description, weight, true);
         this.nutrients = nutrients;
       
     public void use() {
@@ -18,9 +18,8 @@ public class Eating extends Item {
       
   public class Damaging extends Item {
     private int amount;
-
     public Damaging(String name, String description, double weight, int amount) {
-        super(name, description, weight, true, "Damaging");
+        super(name, description, weight, true);
         this.amount = amount;
       
     public void use() {
